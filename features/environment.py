ff1ba76ec32204a38e8ae76d1a0ad5a144b3b1e9
inna <marmaxkat@gmail.com>
2021-04-13 13:48:08 -0700

    environment changed for BrowserStack tests

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,15 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium import webdriver
 from app.application import Application
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+bs_user = 'innaignatyeva2'
+bs_pw = '1Yhv5p8cCn16g6sCEnsV'
 
-def browser_init(context):
+def browser_init(context, test_name):
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
@@ -16,21 +20,32 @@ def browser_init(context):
     # context.driver = webdriver.Firefox(executable_path='/Users/inna/Documents/GitHub/geckodriver')
 
     ### Headless Mode ###
-    options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
-    options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--proxy-server='direct://'")
-    options.add_argument("--proxy-bypass-list=*")
-    options.add_argument("--start-maximized")
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-dev-shm-usage')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--ignore-certificate-errors')
-    context.driver = webdriver.Chrome(executable_path='/Users/inna/Documents/GitHub/chromedriver',chrome_options=options)
+    # options = webdriver.ChromeOptions()
+    # # options.add_argument('headless')
+    # options.add_argument("--window-size=1920,1080")
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--proxy-server='direct://'")
+    # options.add_argument("--proxy-bypass-list=*")
+    # options.add_argument("--start-maximized")
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--ignore-certificate-errors')
+    # context.driver = webdriver.Chrome(executable_path='/Users/inna/Documents/GitHub/chromedriver',chrome_options=options)
 
-    # context.driver.maximize_window()
+    ### BrowserStack ###
+    url = f'http://{bs_user}:{bs_pw}@hub-cloud.browserstack.com/wd/hub'
+    desired_cap = {
+        'browser': 'Chrome',
+        'browser_version': '87.0',
+        'os': 'Windows',
+        'os_version': '10',
+        'name': test_name
+    }
+    context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+
+    context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
     context.driver.wait = WebDriverWait(context.driver, 10)
@@ -40,7 +55,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
